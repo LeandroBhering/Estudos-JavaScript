5e136c6435d355273056de2cdada3ea2af7e4113
LeandroBhering <leandrobenega@gmail.com>
2021-08-04 15:04:16 -0300

    upload de arquivos

---
+++ curso_js/aula12/Ex011.js
@@ -0,0 +1,9 @@
+var idade = 12
+console.log(`A sua idade é de ${idade} anos`)
+if (idade < 18) {
+   console.log ('Você é menor de idade') 
+} else if (idade < 65) {
+        console.log('Você é adulto')
+} else {
+        console.log('Você é idoso')
+}
