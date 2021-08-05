79eb86d9834be45e6f0cd6a1c1046ed23c138605
LeandroBhering <leandrobenega@gmail.com>
2021-08-05 10:38:44 -0300

    commit

--- curso_js/aula14/Exercicio 2/script.js
+++ curso_js/aula14/Exercicio 2/script.js
@@ -4,7 +4,7 @@ function gerarTabuada() {
     var c = 1
     var n = Number(txtn.value)
     res.innerHTML = ''
-    while (c<10) {
+    while (c<=10) {
         var resp = c*n
         res.innerHTML += `<br> ${c} x ${n} = ${resp}`
         c++
