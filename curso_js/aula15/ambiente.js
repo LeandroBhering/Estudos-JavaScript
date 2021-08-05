80609b44dfeed817875ca9a002857d867fb3f3ab
LeandroBhering <leandrobenega@gmail.com>
2021-08-05 13:24:25 -0300

    up

--- curso_js/aula15/ambiente.js
+++ curso_js/aula15/ambiente.js
@@ -7,18 +7,19 @@ console.log(`Nosso vetor é composto pelos valores ${num}`)
 console.log(num.length)
 */
 
-
+/*
 var frutas = ['maça','banana','laranja','pêra','goiaba','amora']
 let cont = 0
-/*
+
 frutas.push('Jabuticaba')
 frutas.unshift('mexirica')
 while (cont < frutas.length) {
     console.log(frutas[cont])
     cont++
 }              
-*/
+
 
 for (cont in frutas) {
    console.log(frutas[cont])
 }
+*/
