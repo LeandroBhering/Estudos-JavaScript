fb147d7f6583be8c211110925ec46e6d6fe10e5e
LeandroBhering <leandrobenega@gmail.com>
2021-08-05 11:59:27 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,24 @@
+/*
+let num = [1,2,3,4,5]
+num[5] = 6
+num.push(7)
+
+console.log(`Nosso vetor é composto pelos valores ${num}`)
+console.log(num.length)
+*/
+
+
+var frutas = ['maça','banana','laranja','pêra','goiaba','amora']
+let cont = 0
+/*
+frutas.push('Jabuticaba')
+frutas.unshift('mexirica')
+while (cont < frutas.length) {
+    console.log(frutas[cont])
+    cont++
+}              
+*/
+
+for (cont in frutas) {
+   console.log(frutas[cont])
+}
