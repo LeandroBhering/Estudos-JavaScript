baf0bce01af4bd925cff9e06666c83de54665c65
LeandroBhering <leandrobenega@gmail.com>
2021-08-11 16:51:32 -0300

    Update aula.js

--- curso2_js/aula.js
+++ curso2_js/aula.js
@@ -1,3 +1,20 @@
-let nome = 'josé'   // Declarar 'let' quando usar escopo local
+/*let nome = 'josé'   // Declarar 'let' quando usar escopo local
 var idade = '20'    // Declarar 'var' quando usar para escopo global
-const imultavel = 0 // Declarar 'const' quando a variável não vai sofrer alteração
+const imultavel = 0 // Declarar 'const' quando a variável não vai sofrer alteração*/
+var cadastro = {
+    nome: ['leandro',],
+    idade: 27,
+    aprovacao: true
+}
+
+cadastro.nome.push('carol','laura','mikhael')
+
+var $nome = 'Milena'
+
+function adicionarNomes(fnome) {
+    return cadastro.nome.push(fnome)
+}
+
+adicionarNomes($nome)
+console.log(cadastro)
+
