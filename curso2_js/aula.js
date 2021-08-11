3b5fda39bf0f8522d1202e383f54a511edacdc58
LeandroBhering <leandrobenega@gmail.com>
2021-08-11 11:47:02 -0300

    upload de arquivos

---
+++ curso2_js/aula.js
@@ -0,0 +1,3 @@
+let nome = 'josé'   // Declarar 'let' quando usar escopo local
+var idade = '20'    // Declarar 'var' quando usar para escopo global
+const imultavel = 0 // Declarar 'const' quando a variável não vai sofrer alteração
