047caed9f10d8990bf9c73953821e032fa95352b
LeandroBhering <leandrobenega@gmail.com>
2021-08-12 10:16:23 -0300

    Update aula.js

--- curso2_js/aula.js
+++ curso2_js/aula.js
@@ -1,8 +1,9 @@
 /*let nome = 'josé'   // Declarar 'let' quando usar escopo local
 var idade = '20'    // Declarar 'var' quando usar para escopo global
 const imultavel = 0 // Declarar 'const' quando a variável não vai sofrer alteração*/
+
 var cadastro = {
-    nome: ['leandro',],
+    nome: ['leandro'],
     idade: 27,
     aprovacao: true
 }
@@ -12,7 +13,7 @@ cadastro.nome.push('carol','laura','mikhael')
 var $nome = 'Milena'
 
 function adicionarNomes(fnome) {
-    return cadastro.nome.push(fnome)
+    cadastro.nome.push(fnome)
 }
 
 adicionarNomes($nome)
