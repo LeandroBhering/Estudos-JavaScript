6be12faacc04cb79c1c47ac7c4a11da9cad0d1b9
LeandroBhering <leandrobenega@gmail.com>
2021-08-12 18:33:16 -0300

    Update script.js

--- curso2_js/aula02/script.js
+++ curso2_js/aula02/script.js
@@ -1,4 +1,4 @@
-var vetor = ['hoje', 'amanha', 'depois',2,3,4]
+/*var vetor = ['hoje', 'amanha', 'depois',2,3,4]
 
 for (let i in vetor){
     console.log(vetor[i])
@@ -6,4 +6,20 @@ for (let i in vetor){
 
 for (let indice of vetor){
     console.log(indice)
+}*/
+var res = fizzBuzz(7)
+console.log(res)
+
+function fizzBuzz(entrada){
+    if (typeof entrada !== 'number'){
+        console.log('Não é um número')
+    } else if (entrada%5 == 0 && entrada%3 == 0) {
+       return console.log('FizzBuzz')
+    } else if (entrada%3 == 0) {
+       return console.log('Fizz')
+    } else if (entrada%5 == 0) {
+        return console.log('Buzz')  
+    } else {
+        console.log(entrada)
+    }
 }
