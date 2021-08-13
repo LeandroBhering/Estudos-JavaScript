a7bebd33a5b8c4596609b9bc84afe13f8cd55d8b
LeandroBhering <leandrobenega@gmail.com>
2021-08-13 09:24:34 -0300

    Update script.js

--- curso2_js/aula02/script.js
+++ curso2_js/aula02/script.js
@@ -71,3 +71,18 @@ function verificarVelocidade(velocidade) {
 }
 */
 
+// Minha Solução
+
+exibirTipo (10)
+
+function exibirTipo (limite){
+    for (let i = 1; i <= limite; i++)
+        if (i%2 == 0) {
+            console.log(i + ' PAR')
+        } else {
+            console.log(i + ' IMPAR')
+        }
+}   
+
+// Solução do Professor - Igual a minha
+
