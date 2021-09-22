94f34399511ce7e89f6a1f8287ee6f5b5c41cfd4
LeandroBhering <leandrobenega@gmail.com>
2021-09-22 12:16:07 -0300

    Update script.js

--- curso2_javaScript/aula02/script.js
+++ curso2_javaScript/aula02/script.js
@@ -10,13 +10,19 @@
 
 
 
-
-
-
-
-
-
-
+/*
+// Inserindo elementos em uma Array
+const numeros = [1,2,3]
+// Inicio
+numeros.unshift(0)
+console.log(numeros)
+// Meio
+numeros.splice(1,0,'a')
+console.log(numeros)
+// Final
+numeros.push(4,5)
+console.log(numeros)
+*/
 
 
 /*
