2f5cef252e9fc0d747818146faa593ba838c3142
LeandroBhering <leandrobenega@gmail.com>
2021-09-22 11:25:47 -0300

    Update script.js

--- curso2_javaScript/aula02/script.js
+++ curso2_javaScript/aula02/script.js
@@ -1,5 +1,24 @@
+function endereco(rua, cidade, cep) {
+    this.rua = rua,
+    this.cidade = cidade,
+    this.cep = cep
+}
 
+const endereco1 = new endereco('a','b','c')
+const endereco2 = new endereco('a','b','c')
 
+function saoIguais(endereco1, endereco2) {
+    if ((endereco1.rua === endereco2.rua) && (endereco1.cidade === endereco2.cidade) && (endereco1.cep === endereco2.cep)) {
+        return console.log(true)
+    }
+    return console.log(false)
+}
+
+function temEnderecoMemoriaIguais (endereco1, endereco2) {
+    
+}
+
+saoIguais(endereco1,endereco2)
 
 
 
