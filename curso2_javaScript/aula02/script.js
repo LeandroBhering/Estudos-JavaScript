1b5bdb804e84cd08d85fda6069129a8ca2e4cf31
LeandroBhering <leandrobenega@gmail.com>
2021-09-22 11:45:33 -0300

    Update script.js

--- curso2_javaScript/aula02/script.js
+++ curso2_javaScript/aula02/script.js
@@ -18,6 +18,23 @@
 
 
 
+
+/*
+function Postagem(titulo, mensagem,autor) {
+    this.titulo = titulo,
+    this.mensagem = mensagem,
+    this.autor = autor,
+    this.visualizacoes = 0,
+    this.comentarios = [],
+    this.estaAoVivo = false
+}
+
+let postagem = new Postagem('a','b','c')
+
+console.log(postagem)
+*/
+
+
 /*
 let postagem = {
     titulo: 'Nova',
