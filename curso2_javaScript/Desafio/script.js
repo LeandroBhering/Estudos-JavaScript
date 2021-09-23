06f9aa8f730b12c1c30d94c093a638625079258a
LeandroBhering <leandrobenega@gmail.com>
2021-09-22 21:55:46 -0300

    Update script.js

--- curso2_javaScript/Desafio/script.js
+++ curso2_javaScript/Desafio/script.js
@@ -1,7 +1,7 @@
 var nomeConvidado = document.getElementById('convidado')
 var nomeConvidou = document.getElementById('quemConvidou')
 var resposta = document.getElementById('resposta')
-const lista = [
+/*const lista = [
     {convidado: 'camila', convidou: 'leandro'},
     {convidado: 'mikhael', convidou: 'leandro'},
     {convidado: 'laura', convidou: 'leandro'},
@@ -9,14 +9,17 @@ const lista = [
     {convidado: 'maria eduarda', convidou: 'leandro'},
     {convidado: 'rodolfo', convidou: 'camila'},
     {convidado: 'ricardo', convidou: 'camila'}
-]
+]*/
+const listaLeandro = ['camila','mikhael','laura','jacqueline','maria eduarda']
+const listaCamila = ['ricardo','rodolfo']
 
 
 function verificar(){
-    for (let i = 0; i <= lista.length; i++){
-        if (nomeConvidou.value == lista[i].convidou) {
-            return resposta.innerHTML = 'Parabéns! Você está na lista de convidados!' 
-        }            
-    }
-            resposta.innerHTML = 'Ops! Parece que você não foi convidado para a festa'
+   if ((nomeConvidou.value == 'leandro') && (listaLeandro.includes(nomeConvidado.value))){
+        resposta.innerHTML = 'Parabéns! Você foi convidado(a) para a festa.'
+   } else if ((nomeConvidou.value == 'camila') && (listaCamila.includes(nomeConvidado.value))){
+        resposta.innerHTML = 'Parabéns! Você foi convidado(a) para a festa.'
+   } else {
+       resposta.innerHTML = 'Ops! Parece que você não foi convidado(a)'
+   }
 }
