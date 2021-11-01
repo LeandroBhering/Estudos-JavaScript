05a95155bca8ee4d5924dab1e2ce8e42e4837ba7
LeandroBhering <leandrobenega@gmail.com>
2021-11-01 17:40:57 -0300

    Change

--- Animacao/load.js
+++ Animacao/load.js
@@ -1,3 +1,16 @@
-var display = document.getElementById('display')
-
-display.innerHTML = bolinha
+const balls = document.querySelectorAll('.js-loadingball')
+TweenMax.staggerFromTo(
+    balls,
+    0.75,
+    {
+        opacity:0,
+        transform: 'scale(0)'
+    },{
+        ease: Power3.easeInOut,
+        opacity:0,
+        transform: 'scale(1.2)',
+        repeat: -1,
+        yoyo: true
+    },
+    2.0
+    )
